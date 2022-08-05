8531d3ffe829c5d2616d38d5d6de5a3df877284e
ajhamaal <ajhamaal@gullysound.net>
2022-08-05 04:39:12 +0300

    first commit

---
+++ postgresql/urbanbaby_prod_DUMP.sql
@@ -0,0 +1,2 @@
+pg_dump: error: connection to server at "urbanbaby-prod-postgresql" (10.98.90.53), port 5432 failed: FATAL:  database "mmwenesi" does not exist
+pod "postgresql-postgresql-ha-client" deleted
